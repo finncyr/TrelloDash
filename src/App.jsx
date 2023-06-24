42635571dbf1ade5602fb569da18a861fb801f7a
Finn <30553137+finncyr@users.noreply.github.com>
2023-06-24 13:05:11 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import { Desktop1 } from "./Desktop1";
+
+export default function App() {
+return (
+    <div>
+    <Desktop1 />
+    </div>
+);
+}
---
+++ src/Desktop1.jsx
@@ -0,0 +1,58 @@
+/* Code generated with AutoHTML Plugin for Figma */
+import "./Desktop1.css";
+
+export const Desktop1 = ({ ...props }) => {
+  return (
+    <div className="desktop-1">
+      <div className="header-bar">
+        <div className="header"></div>
+
+        <div className="trello-dash">TrelloDash</div>
+
+        <div className="project-xxxx">Project: &lcub;XXXX&rcub;</div>
+
+        <div className="raumplan-anzeigen">Raumplan anzeigen</div>
+      </div>
+
+      <div className="progress-block">
+        <div className="rectangle-1"></div>
+
+        <div className="completion-of-tasks">Completion of Tasks</div>
+
+        <div className="rectangle-5"></div>
+
+        <div className="rectangle-6"></div>
+
+        <div className="_12-23-tasks-done">12 / 23 Tasks done</div>
+      </div>
+
+      <div className="spi-mini-block">
+        <div className="spi">SPI</div>
+
+        <div className="_1-103">{spi}</div>
+      </div>
+
+      <div className="sv-block">
+        <div className="rectangle-4"></div>
+
+        <div className="sv">SV</div>
+
+        <div className="_1-2">{sv}</div>
+      </div>
+
+      <div className="ru-mini-block">
+        <div className="rectangle-4"></div>
+
+        <div className="ru">RU</div>
+
+        <div className="_86">{ru}</div>
+      </div>
+
+      <div className="rectangle-2"></div>
+
+      <div className="rectangle-3"></div>
+
+      <div className="zones">Zones</div>
+    </div>
+  );
+};
